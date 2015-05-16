dd90f88cb038a4ab1429c0c983d7ed888abf95a5
honeniq <honeniq@gmail.com>
2015-05-16 15:56:51 +0900

    Implement mareki,akumaeto

---
+++ ruboty-mareki.gemspec
@@ -0,0 +1,9 @@
+Gem::Specification.new do |spec|
+  spec.name          = "ruboty-mareki"
+  spec.version       = "0.0.1"
+  spec.authors       = ["honeniq"]
+  spec.email         = ["honeniq@gmail.com"]
+  spec.summary       = "Translate A.D. to Mareki"
+  spec.files         = [".rb"]
+  spec.require_path  = "."
+end
---
+++ ruboty-mareki.rb
@@ -0,0 +1,32 @@
+require 'ruboty'
+require 'mareki'
+
+module Ruboty
+  module Handlers
+    class Mareki < Base
+      on(
+        /mareki (?<year>.+)/,
+        name: 'mareki',
+        description: 'Translate A.D. to Mareki(魔暦)'
+      )
+
+      on(
+        /akumaeto (?<year>.+)/,
+        name: 'akumaeto',
+        description: 'Answer Akuma-Eto(悪魔干支)'
+      )
+
+      def mareki(message)
+        year = Date.new(message.match_data[1].to_i)
+        message.reply("西暦" + year.year.to_s + "年は、魔暦" + \
+                      year.mareki_year.to_s + "年です。")
+      end
+
+      def akumaeto(message)
+        year = Date.new(message.match_data[1].to_i)
+        message.reply("西暦" + year.year.to_s + "年の、悪魔干支は" + \
+                      year.mareki_eto + "です。")
+      end
+    end
+  end
+end
